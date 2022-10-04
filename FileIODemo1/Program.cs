8818e36497a05d1b0fbdd7aa6406638a1d19841e
VishalKM24 <vkmandal2608@gmail.com>
2022-10-04 09:40:32 +0530

    FileIO

---
+++ FileIODemo1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string data = "But Ankit loves Palak Goel";
+            StreamWriter sw = new StreamWriter("C:\\Users\\Admin\\Desktop\\Framework-Projects\\Sum\\FileIODemo1\\text1.txt", true);
+            sw.WriteLine(data);
+            sw.Close();
+            ReadAll();
+        }
+
+        private static void ReadAll()
+        {
+            StreamReader sw = new StreamReader("C:\\Users\\Admin\\Desktop\\Framework-Projects\\Sum\\FileIODemo1\\text1.txt", true);
+            try
+            {
+                string allData = sw.ReadToEnd();
+                Console.WriteLine(allData);
+            }
+            catch
+            {
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ FileIODemo2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileIODemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            GetAllDrives();
+            
+            GetAllFiles();
+        }
+
+        public static void GetAllFiles()
+        {
+            string[] files = Directory.GetFiles("C:\\Users\\Admin\\Desktop\\Framework-Projects\\Sum\\ContactsManagementsApp");
+            foreach (string file in files)
+            {
+                Console.WriteLine(file);
+            }
+        }
+
+        public static void GetAllDrives()
+        {
+            // get access of all drives
+            DriveInfo[] drivers = DriveInfo.GetDrives();
+            foreach (var drive in drivers)
+            {
+                Console.WriteLine(drive.Name + "\t" + drive.TotalSize + "/" + drive.TotalFreeSpace);
+            }
+        }
+    }
+}
